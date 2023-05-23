8f41dda99705b6dcf62ba609248d4bdc1ee143b8
kyucando <110995347+kyucando@users.noreply.github.com>
2023-05-23 17:43:00 +0900

    MET-8 정규연/2023.05.23/프로젝트 셋팅 _ 도메인 생성

---
+++ src/main/java/com/metain/web/mapper/VacationMapper.java
@@ -0,0 +1,54 @@
+package com.metain.web.mapper;
+
+import com.metain.web.domain.Vacation;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface VacationMapper {
+    /**
+     * 휴가 신청
+     * */
+    public void requestVacation();
+    /**
+     * 부서별 휴가 조회 - 메인 캘린더 표출, 필터
+     * */
+    public List<Vacation> selectListByDept(String empDept);
+    /**
+     * 기간별 휴가 조회
+     * */
+    public List<Vacation> selectListByPeriod();
+    /**
+     * 휴가 전체 조회
+     * */
+    public List<Vacation> selectAllList();
+    /**
+     * 나의 휴가 조회
+     * */
+    public List<Vacation> selectMyList(Long empId);
+    /**
+     * 휴가 승인-관리자
+     * */
+    public void approveVacationRequest(String vacStatus);
+    /**
+     * 휴가 거절-관리자
+     * */
+    public void rejectVacationRequest(String vacStatus);
+    /**
+     * 휴가 취소 - user
+     * */
+    public void cancelVacationRequest(String vacStatus);
+    /**
+     * 휴가 상태변경
+     * */
+    public void changeVacation();
+    /**
+     * 휴가 디테일
+     * */
+    public Vacation vacDetail(Long vacId);
+    /**
+     * 연차별로 휴가 부여하는 기능
+     * */
+    public int vacationCount();
+}
