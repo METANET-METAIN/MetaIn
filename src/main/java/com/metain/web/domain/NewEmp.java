3419597b13303520ec055e8f9134d5f7cb19d0fd
suhyunking <kksshh09211@gmail.com>
2023-05-24 19:54:21 +0900

    MET-9 김수현/2023.05.24/HrMapper Interface 수정 및 NewEmployee -> NewEmp class명 수정

--- src/main/java/com/metain/web/domain/NewEmp.java
+++ src/main/java/com/metain/web/domain/NewEmp.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class NewEmployee {
+public class NewEmp {
     private Long newId;
     private String newName;
     private String newIdnum;
