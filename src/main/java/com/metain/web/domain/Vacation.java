3ab7db63b29b34602ae0c67048f486c9c27a603f
kyucando <110995347+kyucando@users.noreply.github.com>
2023-05-25 17:21:59 +0900

    MET-10 정규연/2023.05.25/vacationlist.html (검색창, grid , layout)

--- src/main/java/com/metain/web/domain/Vacation.java
+++ src/main/java/com/metain/web/domain/Vacation.java
@@ -11,8 +11,7 @@ import java.util.Date;
 @NoArgsConstructor
 public class Vacation {
     private Long vacId;
-    private String vacType;
-    private String vacTybeSub;
+    private String vacType; //일반(반차), 일반(연차), 병가 , 조사(배우자5) ,조사(부모님5),조사(형제1), 예비군
     private Long empId;
     private Date vacRegDate;
     private Date vacStartDate;
