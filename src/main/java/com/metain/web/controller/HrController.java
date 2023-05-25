c625d59609ae3db5cc46ab040f7ccdc1428374b6
suhyunking <kksshh09211@gmail.com>
2023-05-25 14:21:21 +0900

    MET-7 김수현/2023.05.25/layout.html 경로 수정 layout.html css, js 경로 수정

--- src/main/java/com/metain/web/controller/HrController.java
+++ src/main/java/com/metain/web/controller/HrController.java
@@ -1,25 +1,21 @@
 package com.metain.web.controller;
 
-import com.metain.web.domain.Emp;
 import com.metain.web.service.HrService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 public class HrController {
     @Autowired
     private HrService hrService;
 
-    @RequestMapping("/EmpList")
+    @RequestMapping("/hr/emp-list")
     public String selectAll(Model model){
 //        List<Emp> empList = hrService.empList();
 //        model.addAttribute("list",empList);
-        return "hr/EmpList";
+        return "hr/emp-list";
     }
 
 
