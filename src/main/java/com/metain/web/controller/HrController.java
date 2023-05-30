aa70d91be6429d9dd05c2c87fd1304362bdc20b0
seoyyy <seoyyy0306@gmail.com>
2023-05-30 10:27:13 +0900

    MET-20 한서영 0530 / 증명서 레이아웃 작성

--- src/main/java/com/metain/web/controller/HrController.java
+++ src/main/java/com/metain/web/controller/HrController.java
@@ -2,7 +2,7 @@ package com.metain.web.controller;
 
 import com.metain.web.domain.Emp;
 import com.metain.web.service.HrService;
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
