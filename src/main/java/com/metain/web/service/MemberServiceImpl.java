0e0d3dc4cab746773cc1865a403a6728f63da1e7
suhyunking <kksshh09211@gmail.com>
2023-06-05 18:27:33 +0900

    MET-44 김수현/2023.06.05/로그인

---
+++ src/main/java/com/metain/web/service/MemberServiceImpl.java
@@ -0,0 +1,19 @@
+package com.metain.web.service;
+
+import com.metain.web.domain.Emp;
+import com.metain.web.mapper.MemberMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberServiceImpl implements MemberService{
+
+    @Autowired
+    public MemberMapper memberMapper;
+
+
+    @Override
+    public Emp login(Emp emp) {
+        return memberMapper.login(emp);
+    }
+}
