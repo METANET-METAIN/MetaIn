fc201e33e3b0979fd4c73842e21b5f6e62b69221
seoyyy <seoyyy0306@gmail.com>
2023-06-17 20:18:24 +0900

    MET-59 한서영 0617 / 퇴직증명서 신청 수정

--- src/main/java/com/metain/web/dto/CertInfoDTO.java
+++ src/main/java/com/metain/web/dto/CertInfoDTO.java
@@ -3,7 +3,10 @@ package com.metain.web.dto;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
+
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Data
@@ -11,23 +14,38 @@ import java.util.Date;
 @Setter
 public class CertInfoDTO {
 
-    private Long empId;
+    private Long empId; //사원번호
 
     //증명서에 적힐 사원 인적정보
     private String empName;
-    private String empBirth;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date empBirth;
     private String empAddr;
     private String empDetailAddr;
     private String empDept;
     private String empGrade;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date empFirstDt;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date empLastDt;
+
     private String empStatus;
     
     ///증명서에 적힐 해당 증명서정보
-    private Long certSort;
-    private Date certRegDate;
-    private String useOfCert;
-    
+    private String certSort; //증명서종류
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date certRegDate; //발급일자
+    private String useOfCert; //사용용도
+    private String certFileName; //증명서파일이름
+    private Long issueStatus; //발급상태
+
+    private Long empCertId;
+    private Long experCertId;
+    private Long retireCertId;
+
+    private int adjustedId;
+
+
 
 }
