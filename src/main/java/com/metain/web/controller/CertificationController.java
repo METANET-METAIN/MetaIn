0711540218408fd786582e5fa8d78207c962380f
seoyyy <seoyyy0306@gmail.com>
2023-06-17 20:40:16 +0900

    MET-61 한서영 0617 / 증명서파일 생성 및 디지털서명 기능 추가

--- src/main/java/com/metain/web/controller/CertificationController.java
+++ src/main/java/com/metain/web/controller/CertificationController.java
@@ -1,65 +1,59 @@
 package com.metain.web.controller;
 
-import com.metain.web.domain.CommonCert;
+
 import com.metain.web.domain.Emp;
+import com.metain.web.domain.EmpCert;
+import com.metain.web.domain.ExperienceCert;
+import com.metain.web.domain.RetireCert;
+import com.metain.web.dto.CertInfoDTO;
+import com.metain.web.dto.ImageRequestData;
+
 import com.metain.web.service.CertificationService;
+import com.metain.web.service.MyPageService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.io.IOException;
+
+
 import javax.servlet.http.HttpServletRequest;
 
 
-
 @Controller
+@RequestMapping("/certification")
 public class CertificationController {
 
     @Autowired
     private CertificationService certificationService;
-    
+
+
+    @Autowired
+    private MyPageService myPageService;
+
     // 재직증명서 관련
-    @RequestMapping("/certification/emp-cert-show")
-    public String empCertShow(){
+    @RequestMapping("/emp-cert-show")
+    public String empCertShow() {
 
         return "/certification/emp-cert-show";
     }
 
-    
+
     //테스트apply 페이지 연결요청 및 데이터 보내기 테스트
-    @RequestMapping(path = {"/certification/emp-cert-apply","/certification/exper-cert-apply","/certification/retire-cert-apply"})
-    public  String empCertApply(HttpServletRequest request, Model model){
+    @RequestMapping(path = {"/emp-cert-apply", "/exper-cert-apply", "/retire-cert-apply"})
+    public String empCertApply(HttpServletRequest request, Model model) {
 
         // 요청 URL 가져오기
         String requestUrl = request.getRequestURI();
 
-        //재직증명서 신청페이지 나오면 emp객체 넘겨줄거 여기에 작성
-        // 유저 정보가 재직 상태와 퇴사일자를 포함한 가상의 예시 데이터라고 가정
-        
-        //model객체에 데이터 하나씩 넣어줄경우
-//        model.addAttribute("empName", "홍길동");
-//        model.addAttribute("empIdNum", 20163044);
-//        model.addAttribute("empAddr", "광주광역시 광산구");
-//        model.addAttribute("empGrade", "대리");
-//        model.addAttribute("empDept", "개발팀");
-//        model.addAttribute("empFirstDt", 20210824);
-
-
-        //model객체에 객체데이터 넣어서 전달할 경우
-            // empTest 객체 생성 전에 로깅 추가
-            // System.out.println("empTest 객체 생성 전");
-
-        //Emp empTest = new Emp(20163044L, "홍길동", "97년03월06일", "광주광역시 광산구", "대리", "개발팀");
-        // model.addAttribute("empTest", empTest);
-
-            // empTest 객체 생성 후에 로깅 추가
-            //System.out.println("empTest 객체 생성 후 " + empTest.getEmpName());
-    
-        //세션에서 userId값 가져오는 과정  추가하기 ~~
-        //String userId = (String) session.getAttribute("userId");
 
         // 사원의 정보를 데이터베이스에서 조회해서 가져오는 로직
-        Long empId = 5L; //테스트 empId값 (로그인기능완료후 세션값에서 받아오도록 수정하기)
+        Long empId = 108L; //테스트 empId값 (로그인기능완료후 세션값에서 받아오도록 수정하기)
         System.out.println("empInfoList 객체 생성 전");
         Emp empInfoList = certificationService.getEmpInfoList(empId);
         System.out.println("empInfoList 객체 생성 후 " + empInfoList);
@@ -76,96 +70,113 @@ public class CertificationController {
             return "/certification/emp-cert-apply"; // "/certification/emp-cert-apply"에 대한 결과 페이지 반환
         } else if (requestUrl.equals("/certification/exper-cert-apply")) {
             return "/certification/exper-cert-apply"; // "/certification/exper-cert-apply"에 대한 결과 페이지 반환
-        } else if (requestUrl.equals("/certification/retire-cert-apply")){
+        } else if (requestUrl.equals("/certification/retire-cert-apply")) {
             return "/certification/retire-cert-apply"; //"/certification/retire-cert-apply"에 대한 결과 페이지 반환
-        }else {
+        } else {
             return "/index"; // 그 외의 경우의 결과 페이지 반환
         }
 
-       // return "/certification/emp-cert-apply"; //뷰 이름 리턴
     }
-    
-    
-    //apply 정보입력 form에서 정보 넘겨받아서 empcert테이블에 insert할 메소드
+
+
+    //apply 정보입력 form에서 정보 넘겨받아서 각 cert테이블에 insert할 메소드
     @PostMapping("/handleEmpCertInfo")
-    public String handleEmpCertInfo(@ModelAttribute("commonCert")CommonCert commonCert, @RequestParam("certSort") String certSort, @RequestParam("selectedUseOfCert") String selectedUseOfCert, Model model) throws Exception {
-        //empInfoList.setCertSort(certSort);
-        commonCert.setUseOfCert(selectedUseOfCert);
+    public String handleEmpCertInfo(@ModelAttribute("certInfoDTO") CertInfoDTO certInfoDTO, @RequestParam("certSort") String certSort, @RequestParam("selectedUseOfCert") String selectedUseOfCert, Model model) throws Exception {
+
+        System.out.println("certInfo 내용물  " + certInfoDTO);
+        certInfoDTO.setUseOfCert(selectedUseOfCert);
         System.out.println("---------------------> tbl_empcert에 insert할때 필요한 증명서 정보 들어왔나 테스트 !!");
-        System.out.println("---------------------> " + commonCert.getCertSort());
-        System.out.println("---------------------> " + commonCert.getEmpId());
-        System.out.println("---------------------> " + commonCert.getUseOfCert());
+        System.out.println("---------------------> " + certInfoDTO.getCertSort());
+        System.out.println("---------------------> " + certInfoDTO.getEmpId());
+        System.out.println("---------------------> " + certInfoDTO.getUseOfCert());
 
         // 전달된 데이터를 처리하는 로직 작성
         //certificationService.applyEmpCert(empInfoList);
-        
-        //분기 시도
-        if (certSort.equals("A01")){ //증명서종류가 재직증명서일 경우
+
+        //각 증명서별로 분기
+        if (certSort.equals("A01")) { //증명서종류가 재직증명서일 경우
             //EmpCert 테이블에 insert되도록
             System.out.println("!!!!!재직증명서 테이블에 insert된다!!!!!!!!!");
-            certificationService.applyEmpCert(commonCert);
-        } else if (certSort.equals("A02")){ //증명서종류가 경력증명서일 경우
+            EmpCert list = certificationService.applyAndSelectEmpCert(certInfoDTO);
+            model.addAttribute("certList", list);
+
+        } else if (certSort.equals("A02")) { //증명서종류가 경력증명서일 경우
             //ExperCert 테이블에 insert되도록
             System.out.println("!!!!!경력증명서 테이블에 insert된다!!!!!!!!!");
-            certificationService.applyExperCert(commonCert);
-        }else if (certSort.equals("A03")){ //증명서종류가 퇴직증명서일 경우
+            ExperienceCert list = certificationService.applyAndSelectExperCert(certInfoDTO);
+            model.addAttribute("certList", list);
+        } else if (certSort.equals("A03")) { //증명서종류가 퇴직증명서일 경우
             //RetireCert 테이블에 insert되도록
             System.out.println("!!!!!퇴직증명서 테이블에 insert된다!!!!!!!!!");
-            certificationService.applyRetireCert(commonCert);
-        }else {
+            RetireCert list = certificationService.applyAndSelectRetireCert(certInfoDTO);
+            model.addAttribute("certList", list);
+
+        } else {
             System.out.println("!!!!!분기 안됐어 실패!!!!!!!!!");
-            return "index"; //에러창 만들어서 넣기
+            return "/index"; //에러창 만들어서 넣기
         }
 
         // certSort 값을 Model 객체에 추가하여 View로 전달
         model.addAttribute("certSort", certSort);
 
-        return "/certification/emp-cert-complete";
-    }
-
-    @RequestMapping("/certification/emp-cert-complete")
-    public  String empCertComplete(){
-
-
-        return "/certification/emp-cert-complete";
+        //selct해온것도 바로 model객체에 추가해서 view로 보내기 -> 증명서에 값 넣도록
+        return "/certification/cert-complete";
     }
 
 
-    
-    //인쇄 테스트 페이지
-    @RequestMapping("/certification/my-cert-list")
-    public String myCertList(){
-        return "certification/my-cert-list";
+    @RequestMapping("/cert-complete")
+    public String CertComplete(Model model, Long empId, @RequestParam("certSort") String certSort) {
+        return "/certification/cert-complete";
     }
+
+
+    //증명서HTML to PDF 변환 & 디지털 서명 추가
+    @PostMapping("/convertToPdf")
+    @ResponseBody
+    public void convertToPdf(@RequestBody ImageRequestData request) throws IOException {
+
+        certificationService.makeCertPdf(request);
+        //System.out.println("ImageRequestData확인 : ~~~ "+request);
+
+        String certSort = request.getCertSort();
+        Long certId = request.getCertId();
+        //pdf파일에 전자서명 추가한거 저장하기전에 저장해야할 파일명 가져오기
+        String filename = myPageService.getCertFilename(certId, certSort) + ".pdf"; //다운로드할 PDF 파일명 - 디지털서명된 파일이름 empcert같은 객체에서 가져오기
+        System.out.println("디지털서명파일저장할 파일이름 확인 " + filename);
+
+        //저장된 pdf파일에 전자서명 추가하기
+        try {
+            certificationService.signPdf(filename);
+            System.out.println("4!! 디지털서명 완료");
+            //return "success";
+        } catch (Exception e) {
+            System.out.println("디지털서명 실패 ");
+            //return "error";
+        }
+
+    }
+
+
+    @RequestMapping("/mypage/my-cert-list")
+    public String myCertList() {
+        return "/mypage/my-cert-list";
+    }
+
 
     //경력증명서 관련
-    @RequestMapping("/certification/exper-cert-show")
-    public String experCertApply(){
+    @RequestMapping("/exper-cert-show")
+    public String experCertApply() {
 
         return "/certification/exper-cert-show";
     }
 
-//    @RequestMapping("/certification/exper-cert-apply")
-//    public String experCertShow(){
-//
-//        return "/certification/exper-cert-apply";
-//    }
 
-    
     //퇴직증명서 관련
-    @RequestMapping("/certification/retire-cert-show")
-    public String retireCertApply(){
+    @RequestMapping("/retire-cert-show")
+    public String retireCertApply() {
 
         return "/certification/retire-cert-show";
     }
-
-//    @RequestMapping("/certification/retire-cert-apply")
-//    public String retireCertShow(){
-//
-//        return "/certification/retire-cert-apply";
-//    }
-
-
 
 
 }
--- src/main/java/com/metain/web/service/CertificationServiceImpl.java
+++ src/main/java/com/metain/web/service/CertificationServiceImpl.java
@@ -1,24 +1,37 @@
 package com.metain.web.service;
 
-import com.metain.web.domain.CommonCert;
-import com.metain.web.domain.Emp;
-import com.metain.web.domain.EmpCert;
-import com.metain.web.domain.Issue;
+
+import com.groupdocs.signature.Signature;
+import com.groupdocs.signature.exception.GroupDocsSignatureException;
+import com.groupdocs.signature.options.sign.DigitalSignOptions;
+import com.metain.web.domain.*;
+import com.metain.web.dto.CertInfoDTO;
+import com.metain.web.dto.ImageRequestData;
 import com.metain.web.mapper.CertificationMapper;
-import com.metain.web.mapper.HrMapper;
+import com.metain.web.mapper.MyPageMapper;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.Base64;
 
 @Service
-public class CertificationServiceImpl implements CertificationService{
+public class CertificationServiceImpl implements CertificationService {
 
     //Mapper인터페이스 의존성주입
     @Autowired
     private CertificationMapper certificationMapper;
 
+    @Autowired
+    private MyPageMapper myPageMapper;
+
     private final HrService hrService;
+
 
     @Autowired
     public CertificationServiceImpl(HrService hrService) {
@@ -29,51 +42,253 @@ public class CertificationServiceImpl implements CertificationService{
     //로그인한 사원 인사정보 조회
     @Override
     public Emp getEmpInfoList(Long empId) {
+
         Emp empInfolist = hrService.selectEmpInfo(empId);
-        if(empInfolist == null){
+
+
+        if (empInfolist == null) {
             return null;
+        } else {
+
+            String empGrade = empInfolist.getEmpGrade();
+            if ("employee".equals(empGrade)) {
+                empGrade = "사원";
+            } else if ("assistant".equals(empGrade)) {
+                empGrade = "대리";
+            } else if ("manager".equals(empGrade)) {
+                empGrade = "과장";
+            } else if ("deputy".equals(empGrade)) {
+                empGrade = "차장";
+            } else {
+                empGrade = "인사팀/관리자";
+            }
+
+            empInfolist.setEmpGrade(empGrade);
         }
         return empInfolist;
     }
 
 
-
-//재직증명서 발급신청 - 정보입력 
+    //재직증명서 발급신청 - 정보입력
     //재직증명서 신청시 입력정보 추가
     //재직증명서신청시 발급내역 추가
     @Override
-    public int applyEmpCert(CommonCert commonCert) {
+    @Transactional
+    public EmpCert applyAndSelectEmpCert(CertInfoDTO certInfoDTO) { //insert와 그 해당 insert한 행 가져오는 select 트랜젝션처리하기
 
-        certificationMapper.insertEmpCert(commonCert);
-//        //트랜젝션처리하기
-//        certificationMapper.insertEmpCert(empCert);
+        // 이전 쿼리 실행 후 자동으로 생성된 ID 값을 가져옴
+        int lastInsertedId = certificationMapper.getLastEmpCertId();
+
+        // +1을 수행하여 증가시킴
+        int adjustedId = lastInsertedId + 1;
+
+        // certInfoDTO 객체에 adjustedId 설정
+        certInfoDTO.setAdjustedId(adjustedId);
+
+        System.out.println("insert할 내용 확인 : " + certInfoDTO);
+        certificationMapper.insertEmpCert(certInfoDTO);
+
+        // 자동 생성된 키 값을 id 프로퍼티로 가져와서 CertId가지고 해당증명서내역 select하기
+        System.out.println("generated id 확인 : " + certInfoDTO.getEmpCertId());
+        Long certId = certInfoDTO.getEmpCertId();
+
+        EmpCert certlist = certificationMapper.selectEmpCert(certId);
 //        certificationMapper.insertIssue(empCert);
-        return 1;
+
+        // SELECT 결과 처리
+        if (certlist == null) {
+            // 결과가 없는 경우 처리
+            System.out.println("해당하는 EmpCert정보가 없습니다.");
+            return null;
+        } else {
+            // 필요한 작업 수행
+            System.out.println("트랜잭션처리한 함수 확인 : " + certlist);
+            return certlist;
+        }
     }
 
-    public int applyExperCert(CommonCert commonCert){
+    @Transactional
+    public ExperienceCert applyAndSelectExperCert(CertInfoDTO certInfoDTO) {
 
-        certificationMapper.insertExperCert(commonCert);
+        // 이전 쿼리 실행 후 자동으로 생성된 ID 값을 가져옴
+        int lastInsertedId = certificationMapper.getLastExperCertId();
 
-        return 1;
+        // +1을 수행하여 증가시킴
+        int adjustedId = lastInsertedId + 1;
+
+        // certInfoDTO 객체에 adjustedId 설정
+        certInfoDTO.setAdjustedId(adjustedId);
+
+
+        certificationMapper.insertExperCert(certInfoDTO);
+
+        System.out.println("generated id 확인 : " + certInfoDTO.getExperCertId());
+        Long certid = certInfoDTO.getExperCertId();
+
+        ExperienceCert certlist = certificationMapper.selectExperCert(certid);
+
+        // SELECT 결과 처리
+        if (certlist == null) {
+            // 결과가 없는 경우 처리
+            System.out.println("해당하는 ExperienceCert정보가 없습니다.");
+            return null;
+        } else {
+            // 필요한 작업 수행
+            System.out.println("certList확인 : " + certlist);
+            return certlist;
+        }
+
+
     }
 
-    public int applyRetireCert(CommonCert commonCert){
 
-        certificationMapper.insertRetireCert(commonCert);
+    @Transactional
+    public RetireCert applyAndSelectRetireCert(CertInfoDTO certInfoDTO) {
 
-        return 1;
+        // 이전 쿼리 실행 후 자동으로 생성된 ID 값을 가져옴
+        int lastInsertedId = certificationMapper.getLastRetireCertId();
+
+        // +1을 수행하여 증가시킴
+        int adjustedId = lastInsertedId + 1;
+
+        // certInfoDTO 객체에 adjustedId 설정
+        certInfoDTO.setAdjustedId(adjustedId);
+
+
+        certificationMapper.insertRetireCert(certInfoDTO);
+
+        System.out.println("generated id 확인 : " + certInfoDTO.getRetireCertId());
+        Long certId = certInfoDTO.getRetireCertId();
+
+        RetireCert certlist = certificationMapper.selectRetireCert(certId);
+
+
+        // SELECT 결과 처리
+        if (certlist == null) {
+            // 결과가 없는 경우 처리
+            System.out.println("해당하는 RetireCert정보가 없습니다.");
+            return null;
+        } else {
+            // 필요한 작업 수행
+            return certlist;
+        }
     }
 
 
-    
     //증명서 생성 기능
     @Override
-    public EmpCert getEmpCertList(Long empId) {
-       EmpCert list = certificationMapper.selectEmpCert(empId);
-        if(list == null){
+    public EmpCert getEmpCert(Long empId) {
+        EmpCert list = certificationMapper.selectEmpCert(empId);
+        if (list == null) {
             return null;
         }
         return list;
     }
+
+    @Override
+    public ExperienceCert getExperCert(Long empId) {
+        ExperienceCert list = certificationMapper.selectExperCert(empId);
+        if (list == null) {
+            return null;
+        }
+        return list;
+    }
+
+    @Override
+    public RetireCert getRetireCert(Long empId) {
+        RetireCert list = certificationMapper.selectRetireCert(empId);
+        if (list == null) {
+            return null;
+        }
+        return list;
+    }
+
+
+    @Override
+    public void makeCertPdf(ImageRequestData request) throws IOException {
+
+
+        // 이미지 데이터를 Base64로 디코드하여 PDF로 변환
+        byte[] imageBytes = Base64.getDecoder().decode(request.getImageData().split(",")[1]);
+
+//        // 이미지 데이터를 BufferedImage로 변환
+//        ByteArrayInputStream bis = new ByteArrayInputStream(imageBytes);
+//        BufferedImage bufferedImage = ImageIO.read(bis);
+
+        System.out.println("1!! pdf전환 컨트롤러에 요청 들어옴 , 이미지데이터 전달완료 :" + imageBytes);
+
+        // PDF 생성
+        PDDocument document = new PDDocument();
+        PDPage page = new PDPage();
+        document.addPage(page);
+        PDPageContentStream contentStream = new PDPageContentStream(document, page);
+
+        // 이미지 크기 및 위치 설정
+        float targetWidth = 1100; // A4용지 가로 너비 595
+        float targetHeight = 1500; // A4용지 높이 842
+        float imageWidth = request.getImageWidth();
+        float imageHeight = request.getImageHeight();
+
+        System.out.println("이미지크기 : x - " + imageWidth + " , y - " + imageHeight);
+        float scale = Math.min(targetWidth / imageWidth, targetHeight / imageHeight);
+        float offsetX = (targetWidth - imageWidth * scale) / 2 - 240;
+        float offsetY = (targetHeight - imageHeight * scale) / 2 - 355;
+
+
+        contentStream.drawImage(PDImageXObject.createFromByteArray(document, imageBytes, ""), offsetX, offsetY, imageWidth * scale, imageHeight * scale);
+        contentStream.close();
+        System.out.println("2!! pdf생성단계 ");
+
+        String filePath = "src/main/resources/static/certPdfFile/";
+        String fileName = "converted.pdf";
+        // PDF 파일 저장
+        document.save(filePath + fileName);
+        document.close();
+        System.out.println("3!! PDF 전환 및 저장 완료");
+
+
+    }
+
+    @Override
+    public void signPdf(String filename) throws Exception {
+
+
+        try {
+
+            System.out.print(" 디지털서명함수 check 1 /");
+
+            String filePath = "src/main/resources/static/certPdfFile/";
+
+            Signature signature = new Signature(filePath + "converted.pdf");
+            System.out.print(" / 디지털서명함수 check 2" + signature);
+
+
+// 디지털 서명 옵션 정의
+            String certPath = "src/main/resources/certification/";
+            DigitalSignOptions options = new DigitalSignOptions(certPath + "metain.pfx");
+            System.out.print(" / 디지털서명함수 check 3" + options);
+            options.setPassword("12345678900");
+            options.setVisible(true);
+            options.setImageFilePath(filePath + "metain-sign-Image.png");
+            options.setWidth(80);
+            options.setHeight(80);
+            options.setLeft(370);
+            options.setTop(650);
+            options.setPageNumber(1);
+            System.out.print("지장크기확인 : 높이 :" + options.getHeight() + " , 넓이 : " + options.getWidth());
+            System.out.print(" 디지털서명함수 check 4 / ");
+
+
+            // 파일에 문서 서명
+            signature.sign(filePath + filename, options);
+            System.out.print(" 디지털서명함수 check 5 FINAL / ");
+        } catch (Exception e) {
+            System.out.print("에러기록 : " + e.toString());
+            throw new GroupDocsSignatureException(e.toString());
+        }
+
+
+    }//signPdf
+
+
 }
