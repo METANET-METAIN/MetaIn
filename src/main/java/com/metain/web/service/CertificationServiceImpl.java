becad2c752f173892e6b91f5940f0f0fdc586eef
seoyyy <seoyyy0306@gmail.com>
2023-06-20 00:19:14 +0900

    MET-62 한서영 0619 / 증명서 배포 기준으로 수정

--- src/main/java/com/metain/web/service/CertificationServiceImpl.java
+++ src/main/java/com/metain/web/service/CertificationServiceImpl.java
@@ -273,7 +273,8 @@ public class CertificationServiceImpl implements CertificationService {
 
 
 // 디지털 서명 옵션 정의
-            String certPath = "src/main/resources/certification/";
+            //String certPath = "src/main/resources/certification/"; - 로컬용
+            String certPath = "/metainfiles/";  //-배포용
             DigitalSignOptions options = new DigitalSignOptions(certPath + "metain.pfx");
             System.out.print(" / 디지털서명함수 check 3" + options);
             options.setPassword("12345678900");
