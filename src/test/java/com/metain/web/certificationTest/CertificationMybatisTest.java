516f6a05e072dab6900537534c26c21075fd24b1
kyucando <110995347+kyucando@users.noreply.github.com>
2023-06-19 11:20:50 +0900

    MET-66 정규연 /2023-06-19/ 테스트코드 셋팅

---
+++ src/test/java/com/metain/web/certificationTest/CertificationMybatisTest.java
@@ -0,0 +1,25 @@
+package com.metain.web.certificationTest;
+
+import com.metain.web.dto.VacationListDTO;
+import com.metain.web.mapper.VacationMapper;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CertificationMybatisTest {
+    @Autowired
+    private VacationMapper vacMapper;
+   
+    public void selectListByDept(){
+        LocalDate today = LocalDate.now();
+        String empDept="IT";
+        List<VacationListDTO> currMonthVac=vacMapper.selectListByDept(empDept,today);
+        System.out.println(currMonthVac);
+    }
+}
