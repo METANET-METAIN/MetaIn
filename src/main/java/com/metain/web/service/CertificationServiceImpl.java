88b0fce01836ebfd2b6fde38cd1ece076149967a
seoyyy <seoyyy0306@gmail.com>
2023-06-20 12:12:37 +0900

    MET-62 한서영 0619 / 증명서 배포 기준으로 수정

--- src/main/java/com/metain/web/service/CertificationServiceImpl.java
+++ src/main/java/com/metain/web/service/CertificationServiceImpl.java
@@ -262,7 +262,8 @@ public class CertificationServiceImpl implements CertificationService {
 
         try {
 
-            System.out.print(" 디지털서명함수 check 1 /");
+            System.out.print(" 디지털서명함수 check 1 filename도 같이 잘넘어왔나?"+filename +" /");
+
 
             //로컬용 경로
             //String filePath = "src/main/resources/static/certPdfFile/";
@@ -281,7 +282,7 @@ public class CertificationServiceImpl implements CertificationService {
             System.out.print(" / 디지털서명함수 check 3" + options);
             options.setPassword("12345678900");
             options.setVisible(true);
-            options.setImageFilePath(filePath + "metain-sign-Image.png");
+            options.setImageFilePath( filePath + "metain-sign-Image.png");
             options.setWidth(80);
             options.setHeight(80);
             options.setLeft(370);
